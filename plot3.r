31f20ca27237f7712294395177073092894fb6cf
Analyst <44039742+Atifqur@users.noreply.github.com>
2020-03-15 01:43:00 +0500

    Update plot3.r

--- plot3.r
+++ plot3.r
@@ -9,7 +9,7 @@ x<-paste(subset_power$Date, subset_power$Time, sep=" ")
 date_time <- strptime(x,"%d/%m/%Y %H:%M:%S") 
 
 # calling the basic plot function
-png("plot2.png", width=480, height=480)
+png("plot3.png", width=480, height=480)
 plot(date_time, Sub_metering_1, type="l", xlab="",ylab="Energy Sub metering")
 lines(date_time, Sub_metering_2, type="l", col="red")
 lines(date_time, Sub_metering_3, type="l", col="blue")
